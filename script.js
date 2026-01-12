4e1c72fd6381c384a69e5516462f04bfeedf4adb
M.Hassan Tariq <mhassantariqit@gmail.com>
2026-01-12 19:51:10 +0500

    dark mode set and menu page also

---
+++ script.js
@@ -0,0 +1,25 @@
+const sideMenu = document.querySelector("#sidemenu");
+
+function openmenu() {
+  sideMenu.style.right = "0";
+}
+
+function closemenu() {
+  sideMenu.style.right = "-256px";
+}
+
+function toggletheme() {
+  document.documentElement.classList.toggle("dark");
+  if (document.documentElement.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+}
+
+// Check local storage or system preference on load
+if (localStorage.theme === "light") {
+  document.documentElement.classList.remove("dark");
+} else {
+  document.documentElement.classList.add("dark");
+}
